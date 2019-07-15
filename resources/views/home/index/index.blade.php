0f2d528ebdd25a5e4700309b05308a1ac352e54a
菜鸡报道 <31505855+z156728@users.noreply.github.com>
2019-07-15 16:02:39 +0800

    首页编写提交

---
+++ resources/views/home/index/index.blade.php
@@ -0,0 +1,262 @@
+@extends('layout.header')
+@section('content')
+<link rel="stylesheet" href="/css/index.css" />
+<div id="content">
+	<div class="containers">
+		
+		<div class="Popular">
+			<div class="title">热门推荐</div>
+			<div class="lists">
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="mcnLists">
+			<div class="title">MCN机构</div>
+			<div class="lists">
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="setMeal">
+			<div class="title">MCN机构套餐</div>
+			<div class="lists">
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="zimei">
+			<div class="title">自媒体攻略</div>
+			<div class="lists">
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="xinmei">
+			<div class="title">新媒体攻略</div>
+			<div class="lists">
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+				<div class="category">
+					<div class="topItem">
+						<span><img src="/img/12.jpg" alt="" /></span>
+						<span>微视MCN</span>
+					</div>
+					<div class="bottomItem">
+						<p>这事描述内容爱上几个覅很骄傲是和给我个往往更好诶是无公害而华为个个嘿嘿各位黑很反感结完婚 回去覅我好</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+@endsection 
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<title>Document</title>
+</head>
+<link rel="stylesheet" href="/css/reset.css" />
+<link rel="stylesheet" href="/css/header.css" />
+<script src="https://cdn.bootcss.com/jquery/2.2.1/jquery.min.js"></script>
+<body>
+	<div id="bodys">
+		<div id="headItem">
+			<div>
+				<div class="header">
+					<div class="fmName">MCN网红机构</div>
+					<div class="fmName">
+						<a href="">手机号快捷登录</a>
+					</div>
+				</div>
+				<div class="headSearch">
+					<div class="searchItem">
+						<div class="logo">
+							<img src="img/12.jpg">
+						</div>
+						<div class="search">
+							<span><input type="text" /></span>
+							<span>搜索</span>
+						</div>
+					</div>
+				</div>
+				<div class="headClassification">
+					<div class="classification">
+						<ul class="menuList">
+							<li><a href="">首页</a></li>
+							<li><a href="">mcn机构</a></li>
+							<li><a href="">自媒体攻略</a></li>
+							<li><a href="">新媒体攻略</a></li>
+							<li><a href="">意见反馈</a></li>
+							<li><a href="">加盟合作</a></li>
+							<li><a href="">友情链接</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		@yield('content')
+	</div>
+</body>
+</html>
