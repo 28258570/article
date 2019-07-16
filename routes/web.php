612d36327be67a7d47ece9dd86c3d1b10cf88c69
28258570 <15645312656@163.com>
2019-07-16 15:31:05 +0800

    路由文件

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 */
 
 Route::get('/', 'Home\IndexController@index');
+Route::get('/HT', 'Admin\LoginController@index');
 Route::get('home/index/mcnList','Home\IndexController@mcnList');
 Route::resource('admin/admin', 'Admin\AdminController');
 Route::resource('admin/menu', 'Admin\MenuController');
+Route::resource('admin/role', 'Admin\RoleController');
+Route::any('admin/role/role/{id}', 'Admin\RoleController@role');
+Route::any('admin/role/auth', 'Admin\RoleController@auth');
+Route::any('admin/admin/role/{id}', 'Admin\AdminController@role');
+Route::any('admin/login/login', 'Admin\LoginController@login');
+Route::any('admin/login/logout', 'Admin\LoginController@logout');
