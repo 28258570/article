9cec0f06a452bc402b6a4d2984ab3b132a8e8107
菜鸡报道 <31505855+z156728@users.noreply.github.com>
2019-07-17 18:09:50 +0800

    1111

--- resources/views/home/buyDetail/mcnDetail.blade.php
+++ resources/views/home/buyDetail/mcnDetail.blade.php
@@ -1,13 +1,63 @@
 @extends('layout.header')
 @section('content')
-<link rel="stylesheet" href="/css/index.css" />
+<link rel="stylesheet" href="/css/buydetail.css" />
 <div id="content">
 	<div class="containers">
 		
 		<div class="goodDetail">
+			<p class="goodName">MSN机构-全民小视频MCN</p>
+			<!--购买前显示内容-->
+			<div class="details">
+				<div><img src="/img/jigouLists.png" alt="" class="goodImg"/></div>
+				<div>
+					<span>名称：全民小视频mcn</span>
+					<span>介绍：全民小视频23sa啊发顺丰萨芬都是广东省的深V广东省广泛ed各地</span>
+				</div>
+				<div>
+					<span class="price">￥250</span>
+				</div>
+				<div>
+					<span class="payMoment">立即购买</span>
+				</div>
+			</div>
+			<!--购买后显示内容-->
+			<!--<div class="details" style="padding-right: 30px;">
+				<div><img src="/img/jigouLists.png" alt="" class="goodImg"/></div>
+				<div>
+					<span>名称：全民小视频mcn</span>
+					<span>介绍：全民小视频23sa啊发顺丰萨芬都是广东省的深V广东省广泛ed各地</span>
+				</div>
+				<div class="shouhou">
+					<span>专属售后：赵先生</span>
+					<span>联系电话：123456789XX</span>
+				</div>
+				<div>
+					<img src="/img/taocan.png" alt="" class="tiaoma"/>
+				</div>
+			</div>-->
+			
+			
 			
 		</div>
+		
+		
 		<div class="contentDetail">
+			
+			<div class="contents">
+				<p>文章详情</p>
+				<!--购买前显示内容-->
+				<div class="tishi">
+					<span>购买后方可查看</span>
+				</div> 
+				<!--购买后显示内容-->
+				<!--<div class="contentValue">
+					1.asf到官方可都是后的生命过来吗单身快乐每个快递吗赶快买豆腐块莫高窟乱扔昆明人口每个人刚开门 阿魏酸发
+					1.asf到官方可都是后的生命过来吗单身快乐每个快递吗赶快买豆腐块莫高窟乱扔昆明人口每个人刚开门 阿魏酸发
+					1.asf到官方可都是后的生命过来吗单身快乐每个快递吗赶快买豆腐块莫高窟乱扔昆明人口每个人刚开门 阿魏酸发
+					1.asf到官方可都是后的生命过来吗单身快乐每个快递吗赶快买豆腐块莫高窟乱扔昆明人口每个人刚开门 阿魏酸发
+				</div>-->
+			</div>
+			
 			
 		</div>
 		
